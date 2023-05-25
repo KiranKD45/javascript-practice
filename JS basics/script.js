af219cab46170f5d462148b704ecaf60c4b0270f
kiran madiwalar <kiranm81040@gmail.com>
2023-05-25 12:46:13 +0530

    4 q added

--- JS basics/script.js
+++ JS basics/script.js
@@ -4,7 +4,7 @@
 //1. Write a JavaScript program to display the current day and time in the following format.  
 // Sample Output : Today is : Tuesday.
 // Current time is : 10 PM : 30 : 38
-
+//ans ::
 // const toDay = new Date();
 // const day = toDay.getDay();
 // const dayList = ["sunday","monday","tuesuaday","wednesday","thursday","friday","saturday"];
@@ -18,6 +18,24 @@
 // console.log(timeHour+ PmAm+" : "+timeMinute+" : "+timeSecond);
 
 //2. Write a JavaScript program to print the current window contents.
+//ans : 
 //   function Printpage(){
 //     window.print();
 //   }
+// 3. Write a JavaScript program to get the current date.  
+// Expected Output :
+// mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+// ans: 
+//  const date = new Date();
+//  console.log(date.getDate() +"-" + (date.getMonth()+1) + "-" + date.getFullYear());
+
+// 4. Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7
+
+// const side1 = 5;
+// const side2 = 6;
+// const side3 = 7;
+// const semiPerimeter = (side1+side2+side3)/2;
+// const area = Math.sqrt(semiPerimeter * ((semiPerimeter-side1)*(semiPerimeter-side2) *(semiPerimeter-side3)));
+// console.log(area);
+//Area of triangle = sqrt (s*(s-a)*(s-b)*(s-c))
+
