0081759d20ad60e792cdeddeb91e7e7bd7be3b7c
kiran madiwalar <kiranm81040@gmail.com>
2023-05-25 12:20:28 +0530

    initial

---
+++ JS basics/script.js
@@ -0,0 +1,23 @@
+// def: JavaScript is a cross-platform(multiple platform(chrome,firefox,edge,etc...)), object-oriented scripting language. It is a small and lightweight language. Inside a host environment ( a web browser), JavaScript can be connected to the objects of its environment to provide programmatic control over them.
+
+
+//1. Write a JavaScript program to display the current day and time in the following format.  
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+// const toDay = new Date();
+// const day = toDay.getDay();
+// const dayList = ["sunday","monday","tuesuaday","wednesday","thursday","friday","saturday"];
+// const exactday = console.log("Today is : "+dayList[day-1]); 
+
+// const timeHour = toDay.getHours();
+// const PmAm = (timeHour >= 12) ? " PM" : " AM"
+// const timeMinute = toDay.getMinutes();
+// const timeSecond = toDay.getSeconds();
+
+// console.log(timeHour+ PmAm+" : "+timeMinute+" : "+timeSecond);
+
+//2. Write a JavaScript program to print the current window contents.
+//   function Printpage(){
+//     window.print();
+//   }
