69f895e9550c2e637db22756c523a51959092afe
kiran madiwalar <kiranm81040@gmail.com>
2023-06-21 12:43:59 +0530

    21 june

---
+++ Revision(JS+React)/script.js
@@ -0,0 +1,74 @@
+//Revison and practise daily 3 hrs (react aswell as js)
+// 21/06/2023 to 30/06/2023 (javascript+projects)
+// 01/07/2023 to 15/07/2023 (reactjs +projects)
+// 16/06/2023 to 20/07/2023 (redux)
+
+//21/06/2023
+
+//switch case 
+
+// const user = "subadmin"
+// switch (user) {
+//     case "admin":
+//         console.log("access to all things to admin");
+//         break;
+//     case "user" :
+//         console.log("access to content");
+//        break;
+//     case "subadmin" :
+//         console.log("access to test");
+//         break;
+//     default:
+//         console.log("no one is got access");
+//         break;
+// }
+
+
+// if statements
+
+// const user = 17 
+
+// if (user >=18 && user <=80 ) {
+//     console.log("Login successfull");
+// }
+// if (user >=81) {
+//     console.log("you are too old to login");
+// }
+// if (user<18){
+//     console.log("login failed!! Go and study ABCD....");
+// }
+
+// ternary operators
+
+// const age = 10;
+
+// age>=18 || age==10 ? console.log("welcome to goa") :console.log("Sorry cm after 18");
+
+
+// //type of
+// var num = "";
+// console.log(typeof num);
+
+// truthy and falsy
+// js has only 5 falsy values are : NaN,0,"",undefined and null.
+// expect these 5 falsy values all things are in JS are truthy values...
+
+
+// const hello = function check() {
+//     console.log("hiii");
+// }
+
+// const obj = {
+//     name: "kiran",
+//     ph:6361277198
+// }
+
+// var num =`${obj} ${hello}`;
+// if (num) {
+//     console.log("value is truthy");
+// }
+// else{
+//     console.log("value is falsy");
+// }
+
+
